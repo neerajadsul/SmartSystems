b1da91dcc32a1ef41cb636abbee00b1e2479cc55
neerajadsul <neeraj.adsul@gmail.com>
2023-01-10 18:17:42 +0000

    Customises home page, dependencies file

--- SensorNetwork/homewatcher/thlmonitor/views.py
+++ SensorNetwork/homewatcher/thlmonitor/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from datetime import datetime
 
-# Create your views here.
+
+def index(request):
+    context = {
+        'time':datetime.now(),
+    }
+    return render(request, 'thlmonitor/index.html' ,context)
+
