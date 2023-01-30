c406488f5caf65511fd5f827dbce05a19cf68c81
neerajadsul <neeraj.adsul@gmail.com>
2023-01-30 12:39:11 +0000

    view for getting sensor data

--- SensorNetwork/homewatcher/thlmonitor/views.py
+++ SensorNetwork/homewatcher/thlmonitor/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
+
 from datetime import datetime
+import ast
 
 
 def index(request):
@@ -10,3 +13,12 @@ def index(request):
     }
     return render(request, 'thlmonitor/index.html' ,context)
 
+
+@csrf_exempt
+def sensor_data(request):
+    if request.method == 'POST':
+        sbody = request.body.decode()
+        data = ast.literal_eval(sbody)
+        return HttpResponse("Received")
+    if request.method == "GET":
+        return HttpResponse("Server :)")
