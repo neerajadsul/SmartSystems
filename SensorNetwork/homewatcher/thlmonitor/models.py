e77a9c6461e10e169290cdf515ca6296590f1cd0
neerajadsul <neeraj.adsul@gmail.com>
2023-01-31 10:17:55 +0000

    data models to store environmental data and sensor location

--- SensorNetwork/homewatcher/thlmonitor/models.py
+++ SensorNetwork/homewatcher/thlmonitor/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class EnvironmentalData(models.Model):
+    """Represents environmental monitoring data from T, H, Lux sensors"""
+    temperature = models.FloatField(verbose_name='Temperature')
+    humidity = models.FloatField(verbose_name='Relative Humidity')
+    lux_ir = models.IntegerField(verbose_name='IR Intensity')
+    lux_vis_ir = models.IntegerField(verbose_name='Visible+IR Intensity')
+    location = models.ForeignKey('SensorLocation', on_delete=models.PROTECT)
+
+
+class SensorLocation(models.Model):
+    class Floor(models.IntegerChoices):
+        GROUND = 0
+        FIRST = 1
+
+    class GridColumn(models.TextChoices):
+        A = 'A'
+        B = 'B'
+        C = 'C'
+        D = 'D'
+        E = 'E'
+        F = 'F'
+
+    class GridRow(models.IntegerChoices):
+        ONE = 1
+        TWO = 2
+        THREE = 3
+        FOUR = 4
+        FIVE = 5
+        SIX = 6
+        SEVEN = 7
+
+    floor = models.IntegerField(choices=Floor.choices)
+    grid_column = models.CharField(max_length=2, choices=GridColumn.choices)
+    grid_rows = models.IntegerField(choices=GridRow.choices)
+    simple_name = models.CharField(max_length=32)
