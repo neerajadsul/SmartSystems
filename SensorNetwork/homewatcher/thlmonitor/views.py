deb2bf2f965cf80919962ecc005cec83052ce6ed
neerajadsul <neeraj.adsul@gmail.com>
2023-04-26 11:36:11 +0100

    Mechanism to receive sensor data point

--- SensorNetwork/homewatcher/thlmonitor/views.py
+++ SensorNetwork/homewatcher/thlmonitor/views.py
@@ -4,7 +4,9 @@ from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
 
 from datetime import datetime
+import time
 import ast
+from .models import EnvironmentalData, SensorLocation
 
 
 def index(request):
@@ -19,6 +21,25 @@ def sensor_data(request):
     if request.method == 'POST':
         sbody = request.body.decode()
         data = ast.literal_eval(sbody)
+
+        sensor_location = SensorLocation(grid_coordinates = data['location'])
+        sensor_location.save()
+
+        thl_data = EnvironmentalData(
+            temperature=data['T'],
+            humidity = data['RH'],
+            lux_ir = data['LUX'][0],
+            lux_vis_ir = data['LUX'][1],
+            location = sensor_location,
+            timestamp = datetime.fromtimestamp(data['timesecs'])
+        )
+        thl_data.save()
+
         return HttpResponse("Received")
     if request.method == "GET":
-        return HttpResponse("Server :)")
+        print(time.time)
+        return HttpResponse(f"{int(time.time())}")
+
+
+
+
