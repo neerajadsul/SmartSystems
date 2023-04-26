e0cbc13ded605ada00ad1e59d3b71ad042bd539c
neerajadsul <neeraj.adsul@gmail.com>
2023-04-26 15:21:15 +0100

    Refactors X and Y grid step delta to constant variable.

--- SensorNetwork/homewatcher/thlmonitor/static/thlmonitor/map_thldata.js
+++ SensorNetwork/homewatcher/thlmonitor/static/thlmonitor/map_thldata.js
@@ -4,6 +4,9 @@ window.onload = function() {
     draw_ground_floor_rooms();
     draw_first_floor_rooms();
 }
+
+const Xd = 60;
+const Yd = 80;
 function draw_ground_floor() {
     const ground = document.getElementById("ground_floor");
     const gctx = ground.getContext("2d");
@@ -11,12 +14,12 @@ function draw_ground_floor() {
     const H = ground.height;
     gctx.strokeStyle = `rgba(127, 127, 127, 0.1)`;
     gctx.setLineDash([5, 15]);
-    for (let i = 60; i <= W; i+=60) {
+    for (let i = Xd; i <= W; i+=Xd) {
         gctx.moveTo(i, 0)
         gctx.lineTo(i, H);
         gctx.stroke();
     }
-    for (let i = 80; i <= H; i+=80) {
+    for (let i = Yd; i <= H; i+=Yd) {
         gctx.moveTo(0, i)
         gctx.lineTo(W, i);
         gctx.stroke();
@@ -31,12 +34,12 @@ function draw_first_floor() {
     const H = ground.height;
     gctx.strokeStyle = `rgba(127, 127, 127, 0.1)`;
     gctx.setLineDash([5, 15]);
-    for (let i = 60; i <= W; i+=60) {
+    for (let i = Xd; i <= W; i+=Xd) {
         gctx.moveTo(i, 0)
         gctx.lineTo(i, H);
         gctx.stroke();
     }
-    for (let i = 80; i <= H; i+=80) {
+    for (let i = Yd; i <= H; i+=Yd) {
         gctx.moveTo(0, i)
         gctx.lineTo(W, i);
         gctx.stroke();
@@ -50,26 +53,26 @@ function draw_ground_floor_rooms() {
     const H = ground.height;
       // Draw Kitchen Boundary
     gctx.strokeStyle = `rgb(10, 10, 10)`;
-    gctx.moveTo(0, 3*80);
-    gctx.lineTo(W, 3*80);
+    gctx.moveTo(0, 3*Yd);
+    gctx.lineTo(W, 3*Yd);
     gctx.stroke();
-    gctx.moveTo(2*60, 3*80);
-    gctx.lineTo(2*60, H);
+    gctx.moveTo(2*Xd, 3*Yd);
+    gctx.lineTo(2*Xd, H);
     gctx.stroke();
-    gctx.moveTo(1*60, 3*80);
-    gctx.lineTo(1*60, H);
+    gctx.moveTo(1*Xd, 3*Yd);
+    gctx.lineTo(1*Xd, H);
     gctx.stroke();
-    gctx.moveTo(0, 4*80);
-    gctx.lineTo(1*60, 4*80);
+    gctx.moveTo(0, 4*Yd);
+    gctx.lineTo(1*Xd, 4*Yd);
     gctx.stroke();
-    gctx.moveTo(0, 5*80);
-    gctx.lineTo(1*60, 5*80);
+    gctx.moveTo(0, 5*Yd);
+    gctx.lineTo(1*Xd, 5*Yd);
     gctx.stroke();
-    gctx.moveTo(0, 2*80);
-    gctx.lineTo(1*60, 2*80);
+    gctx.moveTo(0, 2*Yd);
+    gctx.lineTo(1*Xd, 2*Yd);
     gctx.stroke();
-    gctx.moveTo(60, 2*80);
-    gctx.lineTo(60, 3*80);
+    gctx.moveTo(Xd, 2*Yd);
+    gctx.lineTo(Xd, 3*Yd);
     gctx.stroke();
 }
 
@@ -81,32 +84,32 @@ function draw_first_floor_rooms() {
       // Draw Kitchen Boundary
     gctx.strokeStyle = `rgb(10, 10, 10)`;
     // Central separator line
-    gctx.moveTo(3*60, 0);
-    gctx.lineTo(3*60, H);
+    gctx.moveTo(3*Xd, 0);
+    gctx.lineTo(3*Xd, H);
     gctx.stroke();
     // Main Bedroom
-    gctx.moveTo(3*60, 3*80);
-    gctx.lineTo(6*60, 3*80);
+    gctx.moveTo(3*Xd, 3*Yd);
+    gctx.lineTo(6*Xd, 3*Yd);
     gctx.stroke();
     // Storage and shower
-    gctx.moveTo(4.25*60, 3*80);
-    gctx.lineTo(4.25*60,4.25*80);
+    gctx.moveTo(4.25*Xd, 3*Yd);
+    gctx.lineTo(4.25*Xd,4.25*Yd);
     gctx.stroke();
-    gctx.moveTo(3*60, 4.25*80);
-    gctx.lineTo(W, 4.25*80);
+    gctx.moveTo(3*Xd, 4.25*Yd);
+    gctx.lineTo(W, 4.25*Yd);
     gctx.stroke();
     // Neeraj Office
-    gctx.moveTo(0, 5*80);
-    gctx.lineTo(3*60, 5*80);
+    gctx.moveTo(0, 5*Yd);
+    gctx.lineTo(3*Xd, 5*Yd);
     gctx.stroke();
     // Family Bath
-    gctx.moveTo(2*60, 2.25*80);
-    gctx.lineTo(2*60, 5*80);
+    gctx.moveTo(2*Xd, 2.25*Yd);
+    gctx.lineTo(2*Xd, 5*Yd);
     gctx.stroke();
-    gctx.moveTo(0, 2.25*80);
-    gctx.lineTo(3*60, 2.25*80);
+    gctx.moveTo(0, 2.25*Yd);
+    gctx.lineTo(3*Xd, 2.25*Yd);
     gctx.stroke();
-    gctx.moveTo(0, 4*80);
-    gctx.lineTo(2*60, 4*80);
+    gctx.moveTo(0, 4*Yd);
+    gctx.lineTo(2*Xd, 4*Yd);
     gctx.stroke();
 }
