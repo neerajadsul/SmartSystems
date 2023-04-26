295e1cc8a7eead89839520d57be617bdbe4b2525
neerajadsul <neeraj.adsul@gmail.com>
2023-04-26 11:35:31 +0100

    Registers models for Temperature, Humidity and Lighting data and sensor locations.

--- SensorNetwork/homewatcher/thlmonitor/admin.py
+++ SensorNetwork/homewatcher/thlmonitor/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import EnvironmentalData, SensorLocation
+
+admin.site.register(SensorLocation)
+admin.site.register(EnvironmentalData)
